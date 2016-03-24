e72f8410cabd5773d316b5e25ef8f7c0ef4fff1a
jeffreyyourman <jeffreyyourman@gmail.com>
2016-03-24 12:25:42 -0400

    modified files

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,6 +8,7 @@ $('#addTrainButton').on('click', function(){
     var timeInput = $('#timeInput').val().trim();
     var frequencyInput = $('#frequencyInput').val().trim();
 
+
     //creates local "temporary" object for holding train data
     var newTrain = {
       name: trainName,
@@ -33,7 +34,37 @@ $('#addTrainButton').on('click', function(){
     //prevents moving to new page
     return false;
 });
-
-employee.on('child_added', function(childSnapshot, prevChildKey){
+// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+trainData.on('child_added', function(childSnapshot, prevChildKey){
     console.log(childSnapshot.val());
+
+    //store everything into a variable
+    var trainName = childSnapshot.val().name;
+    var destinationInput = childSnapshot.val().destination;
+    var timeInput = childSnapshot.val().time;
+    var frequencyInput = childSnapshot.val().frequency;
+
+    // Employee Info
+    console.log(trainName);
+    console.log(destinationInput);
+    console.log(timeInput);
+    console.log(frequencyInput);
+
+    //prettify the train destination
+    var trainFrequencyPretty = moment();
+    trainFrequencyPretty = moment(timeInput).format('hh:mm');
+    console.log(trainFrequencyPretty);
+    var nextArrival = moment().diff(moment.unix(frequencyInput, 'HH:mm'),"minutes");
+    console.log(nextArrival); 
+    var minutesAway = frequencyInput * nextArrival;
+
+    $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + destinationInput + "</td><td>" + frequencyInput + "</td><td>"  + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
+
+
+
+
+
+
+
+
 })
